9df250db8cc18ce99a14592a07712bc5f7e49e07
simonbeirouti <hello@simonbeirouti.com>
2022-10-16 21:37:36 +1100

    create /api/jokes route to get all jokes

---
+++ pages/api/jokes.js
@@ -0,0 +1,13 @@
+import { getJokes } from "../../helpers/getJokes";
+
+export default async function handler(req, res) {
+  let jokes;
+
+  if (req.method === "GET") {
+    jokes = await getJokes();
+  } else {
+    jokes = { error: "Method not allowed" };
+  }
+
+  res.status(200).json(jokes);
+}
