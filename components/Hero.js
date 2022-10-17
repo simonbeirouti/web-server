fa7f45b6090ae37bed4d0d3037c6324854e341f8
simonbeirouti <hello@simonbeirouti.com>
2022-10-17 20:31:05 +1100

    turn hero into reuseable component

--- components/Hero.js
+++ components/Hero.js
@@ -1,20 +1,19 @@
 import CTA from "./CTA";
 
-export default function Hero() {
+export default function Hero({ introduction, benefits, information }) {
   return (
     <div className="overflow-x-hidden">
       <section className="relative py-12 sm:py-16 lg:pt-20 xl:pb-0">
         <div className="relative px-4 mx-auto sm:px-6 lg:px-8 max-w-7xl">
           <div className="max-w-3xl mx-auto text-center">
             <p className="inline-flex px-4 py-2 text-base text-gray-900 border border-gray-200 rounded-full font-pj">
-              Made by Developers, for Developers
+              {introduction}
             </p>
             <h1 className="mt-5 text-4xl font-bold leading-tight text-gray-900 sm:text-5xl sm:leading-tight lg:text-6xl lg:leading-tight font-pj">
-              Quality resources shared by the community
+              {benefits}
             </h1>
             <p className="max-w-md mx-auto mt-6 text-base leading-7 text-gray-600 font-inter">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vehicula
-              massa in enim luctus. Rutrum arcu.
+              {information}
             </p>
 
             <CTA />
