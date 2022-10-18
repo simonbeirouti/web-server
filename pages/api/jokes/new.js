f90b35a627553da000ba329403108fbefd62088b
simonbeirouti <hello@simonbeirouti.com>
2022-10-18 19:14:25 +1100

    change function name and change error message

--- pages/api/jokes/new.js
+++ pages/api/jokes/new.js
@@ -1,6 +1,6 @@
 import { postJoke } from "../../../helpers/getJokes";
 
-export default async function handler(req, res) {
+export default async function newJoke(req, res) {
   let jokeResult;
 
   if (req.method === "POST") {
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     try {
       res.status(200).json({ ...jokeResult, message: `Joke created` });
     } catch (error) {
-      res.status(400).json({ ...jokeResult, message: `Joke created` });
+      res.status(400).json({ message: `Make some changes to the request` });
     }
   } else {
     res.status(405).json({ error: `Method ${req.method} not allowed` });
