2e01c070a4936bcc856f39f91955a5ff25ac5aae
simonbeirouti <hello@simonbeirouti.com>
2022-10-19 11:38:02 +1100

    fix jokes get request

--- pages/api/jokes/index.js
+++ pages/api/jokes/index.js
@@ -1,13 +1,18 @@
-import { getJokes } from "../../../helpers/getJokes";
+import { supabase } from "../../../utils/supabase";
 
 export default async function handler(req, res) {
-  let jokes;
-
   if (req.method === "GET") {
-    jokes = await getJokes();
+    const { data: jokes, error } = await supabase
+      .from("jokes")
+      .select("id, joke, details(name)")
+      .order("id", { ascending: true });
+    if (error) {
+      res.status(400).json({ error: error.message });
+    }
+    if (jokes) {
+      res.status(200).json(jokes);
+    }
   } else {
-    jokes = { error: "Method not allowed" };
+    res.status(405).json({ error: `Method ${req.method} not allowed` });
   }
-
-  res.status(200).json(jokes);
 }
